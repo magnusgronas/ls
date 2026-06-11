6ff3323a0200a3b276d270abd81da6cf9f09503c
Magnus Grønås <magnus.s.gronas@gmail.com>
2026-06-11 11:48:45 +0200

    basic ls functionality

--- ls.c
+++ ls.c
@@ -1,6 +1,23 @@
+#include <dirent.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("this program will list directories\n");
+    const char *path = argc > 1 ? argv[1] : ".";
+
+    DIR *dir = opendir(path);
+    if (!dir) {
+        perror("opendir");
+        return 1;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_name[0] == '.')
+            continue;
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+
     return 0;
 }
