b18c049a24a3a03039e4f2d0af294817503a4d08
Magnus Grønås <magnus.s.gronas@gmail.com>
2026-06-11 19:34:47 +0200

    added the -a option from ls

--- ls.c
+++ ls.c
@@ -1,8 +1,27 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <unistd.h>
+
+int show_all = 0;
 
 int main(int argc, char *argv[]) {
-    const char *path = argc > 1 ? argv[1] : ".";
+
+    int opt;
+    while ((opt = getopt(argc, argv, "ab")) != -1) {
+        switch (opt) {
+        case 'a':
+            show_all = 1;
+            break;
+        case 'b':
+            printf("-b gitt som argument\n");
+            break;
+        default:
+            fprintf(stderr, "usage: %s [-a] [path]\n", argv[0]);
+            return 1;
+        }
+    }
+
+    const char *path = optind < argc ? argv[optind] : ".";
 
     DIR *dir = opendir(path);
     if (!dir) {
@@ -12,7 +31,7 @@ int main(int argc, char *argv[]) {
 
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_name[0] == '.')
+        if (!show_all && entry->d_name[0] == '.')
             continue;
         printf("%s\n", entry->d_name);
     }
